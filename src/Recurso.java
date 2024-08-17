51fc1bc1af26643fecec58e9725c418ecb258dd7
mynor hidalgo <stmhida6@upana.edu.gt>
2024-08-16 22:50:13 -0600

    Inicio

---
+++ src/Recurso.java
@@ -0,0 +1,11 @@
+class Recurso {
+    // clase  dummyque representa un recurso
+    private final String nombre;
+
+    public Recurso(String nombre) {
+        // Inicializar el recurso con unnombre
+        this.nombre = nombre;
+    }
+
+
+}
